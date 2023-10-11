f2feeb1261286131780f3f0a7444c0effabf3434
Mehdi_ha01 <mehdizadeorxan2000@gmail.com>
2023-10-11 15:42:38 +0400

    js css guncel

--- Js/event-color-div.js
+++ Js/event-color-div.js
@@ -1,27 +1,46 @@
-let divX=0;
-let divY=0;
-let onFly = false
+const block1=document.getElementById("block1");
+const block2=document.getElementById("block2");
+const block3=document.getElementById("block3");
 
-const myDiv=(divkey)=>{
-    
 
-}
+function myDivas(mousebtn,divValue,clientx,clienty){
+    const speed = 2;
 
-const divPrint=()=>{
-    window.onmousedown = (e)=>{
-        switch (e.which){
-            case 1:
-                myDiv(1)
-                break;
-            case 2:
-                myDiv(2)
-                break;
-            case 3:
-                myDiv(3)
-                break;
+    if(mousebtn===1 || mousebtn===2 || mousebtn===3){
+        const targetcoordinateX= clientx-divValue.clientWidth/2;
+        const targetcoordinateY= clienty-divValue.clientHeight/2;
+
+        let divX=parseFloat(getComputedStyle(divValue).left);
+        let divY=parseFloat(getComputedStyle(divValue).top);
+
+        const x=(targetcoordinateX-divX)/speed;
+        const y=(targetcoordinateY-divY)/speed;
+        function divAnimate (){
+            if(Math.abs(divX-targetcoordinateX)>1||Math.abs(divY-targetcoordinateY)>1){
+                divX+=x;
+                divY+=y;
+                divValue.style.left=`${divX}px`;
+                divValue.style.top=`${divY}px`;
+                setTimeout(divAnimate,10)
+            }
         }
-        window.block1.oncontextmenu = ()=> false
-        window.block2.oncontextmenu = ()=> false
-        window.block3.oncontextmenu = ()=> false
+        divAnimate();
+
+
     }
+
 }
+
+document.addEventListener("mousedown",(e)=>{
+    switch (e.button){
+        case 0:
+            myDivas(1, block1, e.clientX, e.clientY);
+            break;
+        case 1:
+            myDivas(2, block2, e.clientX, e.clientY);
+            break;
+        case 2:
+            myDivas(3, block3, e.clientX, e.clientY);
+            break;
+    }
+})
